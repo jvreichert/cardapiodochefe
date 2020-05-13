56c62131ded5a7abdcde3560f735b61e496d4654
Reichert, J. V. M <39267276+jvreichert@users.noreply.github.com>
2020-05-13 11:33:49 -0300

    criacao cardapio

--- js/messenger.js
+++ js/messenger.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
-    $(document).on("click", "#fazer-pedido", function(){
-        var whatsappMessage= "*Pedido via Site*"+"\r\n\r\n"+"Pedido.......";
+    let sabor = "";
+    $(document).on("click", ".btnEscolhePizza", function(){
+      sabor = $(this).closest(".card-pizza").find(".sabor-pizza").text();
+      alert(sabor);
+      $("#divSabores").slideUp();
+      $("#divFinalizaPedido").slideDown();
+    }).on("click", "#fazer-pedido", function(){
+        $("#divFinalizaPedido").show();
+        let nome = $("#nome_pedido").val();
+        let telefone = $("#telefone_pedido").val();
+        let endereco = $("#endereco_pedido").val();
+        let pagamento = $('input[name="paymentMethod"]:checked').val();
+        let troco = $("#troco_pedido").val();
+
+        var whatsappMessage= "*Pedido via Site*"+"\r\n\r\n"
+        +"Sabor: "+ sabor
+        +"Cliente: "+ nome
+        +"Telefone: "+ telefone
+        +"Endereço de entrega: "+ endereco
+        +"Pagamento: "+ pagamento
+        +"Troco: "+ troco;
 
         whatsappMessage = window.encodeURIComponent(whatsappMessage);
         window.open('https://api.whatsapp.com/send?phone=+5517988219946&text='+whatsappMessage, '_blank');
