d07da72fc8a1a714fd8077f7529f5ed787962043
Your Name <git@github.com>
2020-05-22 16:30:19 -0300

    adicional promoçao

--- js/messenger.js
+++ js/messenger.js
@@ -5,16 +5,31 @@ $(document).ready(function () {
     let meia2 = "";
     let inteira = "";
     let preco = 0;
-    $(document).on("click", ".btnEscolheMeiaBroto", function () {
+    let precoPromocao = 0;
+    let promocao = false;
+
+    $(document)
+        .on("click", ".btnEscolheMeiaBroto", function () {
             if (meia1 == "") {
-                meia1 = $(this).closest(".card-pizza").find(".sabor-pizza").text();
-                bootbox.alert("Você adicionou " + meia1 + "(meia broto) ao carrinho");
+                meia1 = $(this)
+                    .closest(".card-pizza")
+                    .find(".sabor-pizza")
+                    .text();
+                bootbox.alert(
+                    "Você adicionou " + meia1 + "(meia broto) ao carrinho"
+                );
                 $(".btnEscolheInteiraBroto").slideUp();
                 $(".div-grande").slideUp();
+                $(".divAdicionais").slideUp();
                 preco += parseFloat($(this).attr("data-preco"));
             } else if (meia2 == "") {
-                meia2 = $(this).closest(".card-pizza").find(".sabor-pizza").text();
-                bootbox.alert("Você adicionou " + meia2 + "(meia broto) ao carrinho");
+                meia2 = $(this)
+                    .closest(".card-pizza")
+                    .find(".sabor-pizza")
+                    .text();
+                bootbox.alert(
+                    "Você adicionou " + meia2 + "(meia broto) ao carrinho"
+                );
                 preco += parseFloat($(this).attr("data-preco"));
                 carrinho.push({
                     tipo: "Broto",
@@ -30,6 +45,7 @@ $(document).ready(function () {
                 preco = 0;
                 $(".btnEscolheInteiraBroto").slideDown();
                 $(".div-grande").slideDown();
+                $(".divAdicionais").slideDown();
                 console.log(carrinho);
             } else {
                 alert("Ocorreu um erro, por favor escolha novamente.");
@@ -38,16 +54,17 @@ $(document).ready(function () {
             // carrinho.push({});
         })
         .on("click", ".btnEscolheInteiraBroto", function () {
-            inteira = $(this).closest(".card-pizza").find(".sabor-pizza").text();
-            bootbox.alert("Você adicionou " + inteira + "(inteira broto) ao carrinho");
+            inteira = $(this)
+                .closest(".card-pizza")
+                .find(".sabor-pizza")
+                .text();
+            bootbox.alert(
+                "Você adicionou " + inteira + "(inteira broto) ao carrinho"
+            );
             preco += parseFloat($(this).attr("data-preco"));
-            carrinho.push({
-                tipo: "Broto",
-                meia1: meia1,
-                meia2: meia2,
-                inteira: inteira,
-                preco: preco
-            })
+            carrinho.push(
+                {tipo: "Broto", meia1: meia1, meia2: meia2, inteira: inteira, preco: preco}
+            )
 
             meia1 = "";
             meia2 = "";
@@ -56,16 +73,15 @@ $(document).ready(function () {
             console.log(carrinho);
         })
         .on("click", ".btnEscolhe", function () {
-            inteira = $(this).closest(".card-pizza").find(".sabor-pizza").text();
+            inteira = $(this)
+                .closest(".card-pizza")
+                .find(".sabor-pizza")
+                .text();
             bootbox.alert("Você adicionou " + inteira + " ao carrinho");
             preco += parseFloat($(this).attr("data-preco"));
-            carrinho.push({
-                tipo: "-",
-                meia1: meia1,
-                meia2: meia2,
-                inteira: inteira,
-                preco: preco
-            })
+            carrinho.push(
+                {tipo: "-", meia1: meia1, meia2: meia2, inteira: inteira, preco: preco}
+            )
 
             meia1 = "";
             meia2 = "";
@@ -73,18 +89,39 @@ $(document).ready(function () {
             preco = 0;
             console.log(carrinho);
         })
-
         .on("click", ".btnEscolheMeiaGrande", function () {
             if (meia1 == "") {
-                meia1 = $(this).closest(".card-pizza").find(".sabor-pizza").text();
-                bootbox.alert("Você adicionou " + meia1 + "(meia grande) ao carrinho");
+                if ($(this).hasClass("tagPromocao")) {
+                    promocao = true;
+                    precoPromocao = parseFloat($(this).attr("data-preco_promocao"))
+                }
+                meia1 = $(this)
+                    .closest(".card-pizza")
+                    .find(".sabor-pizza")
+                    .text();
+                bootbox.alert(
+                    "Você adicionou " + meia1 + "(meia grande) ao carrinho"
+                );
                 $(".btnEscolheInteiraGrande").slideUp();
                 $(".div-broto").slideUp();
+                $(".divAdicionais").slideUp();
                 preco += parseFloat($(this).attr("data-preco"));
             } else if (meia2 == "") {
-                meia2 = $(this).closest(".card-pizza").find(".sabor-pizza").text();
-                bootbox.alert("Você adicionou " + meia2 + "(meia grande) ao carrinho");
-                preco += parseFloat($(this).attr("data-preco"));
+                if ($(this).hasClass("tagPromocao") && promocao) {
+                    precoPromocao += parseFloat($(this).attr("data-preco_promocao"))
+                    preco = precoPromocao
+                } else {
+                    preco += parseFloat($(this).attr("data-preco"));
+                }
+
+                meia2 = $(this)
+                    .closest(".card-pizza")
+                    .find(".sabor-pizza")
+                    .text();
+                bootbox.alert(
+                    "Você adicionou " + meia2 + "(meia grande) ao carrinho"
+                );
+
                 carrinho.push({
                     tipo: "Grande",
                     meia1: meia1,
@@ -99,6 +136,7 @@ $(document).ready(function () {
                 preco = 0;
                 $(".btnEscolheInteiraGrande").slideDown();
                 $(".div-broto").slideDown();
+                $(".divAdicionais").slideDown();
                 console.log(carrinho);
             } else {
                 alert("Ocorreu um erro, por favor escolha novamente.");
@@ -106,16 +144,17 @@ $(document).ready(function () {
             }
         })
         .on("click", ".btnEscolheInteiraGrande", function () {
-            inteira = $(this).closest(".card-pizza").find(".sabor-pizza").text();
-            bootbox.alert("Você adicionou " + inteira + "(inteira grande) ao carrinho");
+            inteira = $(this)
+                .closest(".card-pizza")
+                .find(".sabor-pizza")
+                .text();
+            bootbox.alert(
+                "Você adicionou " + inteira + "(inteira grande) ao carrinho"
+            );
             preco += parseFloat($(this).attr("data-preco"));
-            carrinho.push({
-                tipo: "Grande",
-                meia1: meia1,
-                meia2: meia2,
-                inteira: inteira,
-                preco: preco
-            })
+            carrinho.push(
+                {tipo: "Grande", meia1: meia1, meia2: meia2, inteira: inteira, preco: preco}
+            )
 
             meia1 = "";
             meia2 = "";
@@ -138,26 +177,27 @@ $(document).ready(function () {
                 } else {
                     nomeProduto = element.inteira
                 }
-                $("#ul-carrinho").append('<li class="list-group-item d-flex justify-content-between lh-condensed itens-carrinho">' +
-                    '<div>' +
-                    '<h6 class="my-0">' + nomeProduto + '</h6>' +
-                    '<small class="text-muted">' + element.tipo + '</small>' +
-                    '</div>' +
-                    '<span class="text-muted">R$ ' + element.preco + '</span>' +
-                    '</li>')
+                $("#ul-carrinho").append(
+                    '<li class="list-group-item d-flex justify-content-between lh-condensed itens-c' +
+                    'arrinho"><div><h6 class="my-0">' + nomeProduto + '</h6><small class="text-mute' +
+                    'd">' + element.tipo + '</small></div><span class="text-muted">R$ ' +
+                    element.preco + '</span></li>'
+                )
                 total += element.preco;
             })
 
             $("#valor-total").html("R$ " + parseFloat(total))
         })
         .on("click", "#fazer-pedido", function () {
-            if (!validator())
+            if (!validator()) 
                 return false;
-
+            
             $("#divFinalizaPedido").show();
             let nome = $("#nome_pedido").val();
             let telefone = $("#telefone_pedido").val();
-            let endereco = $('input[name="metodoEntrega"]:checked').val() + "\n" + $("#endereco_pedido").val();
+            let endereco = $('input[name="metodoEntrega"]:checked').val() + "\n" + $(
+                "#endereco_pedido"
+            ).val();
             let pagamento = $('input[name="paymentMethod"]:checked').val();
             let troco = $("#troco_pedido").val();
             let bairro = $("#bairro_pedido option:selected").text();
@@ -166,26 +206,24 @@ $(document).ready(function () {
 
             carrinho.forEach(function (element, index) {
                 if (element.inteira == "") {
-                    sabor += "01 " + element.meia1 + " e " + element.meia2 + "(" + element.tipo + ")" + "\n"
+                    sabor += "01 " + element.meia1 + " e " + element.meia2 + "(" + element.tipo + ")\n"
                 } else {
-                    sabor += "01 " + element.inteira + "(" + element.tipo + ")" + "\n"
+                    sabor += "01 " + element.inteira + "(" + element.tipo + ")\n"
                 }
             });
 
-            var whatsappMessage = "*Pedido via Site*" + "\r\n\r\n" +
-                "- Sabor(es): " + sabor + "\n" +
-                "- Cliente: " + nome + "\n" +
-                "- Telefone: " + telefone + "\n" +
-                "- Entrega: " + endereco + "\n" +
-                "- Bairro: " + bairro + "\n" +
-                "- Preço Entrega: R$" + valorFrete + "\n" +
-                "- Pagamento: " + pagamento + "\r\n\r\n" +
-                "- Total: " + $("#valor-total").text() + "\n" +
-                "- Troco: " + troco + "\n" +
-                "- **Observações: " + obs;
+            var whatsappMessage = "*Pedido via Site*\r\n\r\n- Sabor(es): " + sabor + "\n- C" +
+                    "liente: " + nome + "\n- Telefone: " + telefone + "\n- Entrega: " +
+                    endereco + "\n- Bairro: " + bairro + "\n- Preço Entrega: R$" +
+                    valorFrete + "\n- Pagamento: " + pagamento + "\r\n\r\n- Total: " + $(
+                "#valor-total"
+            ).text() + "\n- Troco: " + troco + "\n- **Observações: " + obs;
 
             whatsappMessage = window.encodeURIComponent(whatsappMessage);
-            window.open('https://api.whatsapp.com/send?phone=+5517991055329&text=' + whatsappMessage, '_blank');
+            window.open(
+                'https://api.whatsapp.com/send?phone=+5517991055329&text=' + whatsappMessage,
+                '_blank'
+            );
         })
         .on("change", 'input[name="metodoEntrega"]', function () {
             if ($('input[name="metodoEntrega"]:checked').val() == "delivery") {
@@ -214,26 +252,23 @@ $(document).ready(function () {
                 } else {
                     nomeProduto = element.inteira
                 }
-                $("#ul-carrinho").append('<li class="list-group-item d-flex justify-content-between lh-condensed itens-carrinho">' +
-                    '<div>' +
-                    '<h6 class="my-0">' + nomeProduto + '</h6>' +
-                    '<small class="text-muted">' + element.tipo + '</small>' +
-                    '</div>' +
-                    '<span class="text-muted">R$ ' + element.preco + '</span>' +
-                    '</li>')
+                $("#ul-carrinho").append(
+                    '<li class="list-group-item d-flex justify-content-between lh-condensed itens-c' +
+                    'arrinho"><div><h6 class="my-0">' + nomeProduto + '</h6><small class="text-mute' +
+                    'd">' + element.tipo + '</small></div><span class="text-muted">R$ ' +
+                    element.preco + '</span></li>'
+                )
                 total += element.preco;
             })
 
-            $("#ul-carrinho").append('<li class="list-group-item d-flex justify-content-between lh-condensed frete-carrinho">' +
-                '<div>' +
-                '<h6 class="my-0">Entrega</h6>' +
-                '</div>' +
-                '<span class="text-muted">R$ ' + $(this).val() + '</span>' +
-                '</li>')
+            $("#ul-carrinho").append(
+                '<li class="list-group-item d-flex justify-content-between lh-condensed frete-c' +
+                'arrinho"><div><h6 class="my-0">Entrega</h6></div><span class="text-muted">R$ ' +
+                $(this).val() + '</span></li>'
+            )
 
             $("#valor-total").html("R$ " + parseFloat(total + parseFloat($(this).val())))
         });
-
 
     let validator = () => {
         if ($("#nome_pedido").val() == "") {
